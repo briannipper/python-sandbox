7d6711c3ee20eae6e813dab17b19c433a827f3ac
brian nipper <brian.nipper@keends.com>
2023-01-11 10:26:29 -0500

    select option then run command

--- sqs_main.py
+++ sqs_main.py
@@ -1,19 +1,45 @@
-import time
 from sqs_create_with_deadletter import *
 from sqs_purge_then_delete import *
 from sqs_create_message import *
+from pick import pick
+import os
 
 print()
 
-print("Please enter a queue name:")
+title = "Please select an operation: "
+options = ["Create Queue", "Delete Queue", "Send Message"]
 
-queue_name = input()
+option, index = pick(options, title)
 
+print("You selected: " + option)
 print()
-print("Working with queue " + queue_name)
-print()
 
-create_with_deadletter(queue_name)
-send_message(queue_name)
-time.sleep(120)
-purge_then_delete(queue_name)
+if index == 0:
+    print("Please enter a queue name:")
+    queue_name = input()
+
+    print()
+    print("Create Queue: " + queue_name)
+
+    create_with_deadletter(queue_name)
+
+if index == 1:
+    print("Please enter a queue name:")
+    queue_name = input()
+
+    print()
+    print("Delete Queue: " + queue_name)
+
+    purge_then_delete(queue_name)
+
+if index == 2:
+    print("Please enter a queue name:")
+    queue_name = input()
+
+    print()
+    print("Send Message to Queue: " + queue_name)
+
+    send_message(queue_name)
+
+print("Press any key to exit")
+os.system("read")
