a6d72abc6589decd24be6734e835a8730a5659f2
brian nipper <brian.nipper@keends.com>
2023-02-13 11:26:00 -0500

    use main and introduce ses

--- main.py
+++ main.py
@@ -2,35 +2,51 @@ from sqs_create_with_deadletter import *
 from sqs_purge_then_delete import *
 from sqs_create_message import *
 from sqs_receive_messages import *
+from ses_core import *
 from pick import pick
 import os
+
+def get_queue_name():
+    print("Please enter a queue name:")
+    result = input()
+
+    print()
+    print(option + ": " + result)
+    return result
 
 print()
 
 title = "Please select an operation: "
-options = ["Create Queue", "Delete Queue", "Send Message To Queue", "Receive Messages From Queue"]
+options = [
+    "Send Email",
+    "Create Queue",
+    "Delete Queue",
+    "Send Message To Queue",
+    "Receive Messages From Queue",
+]
 
 option, index = pick(options, title)
 
 print("You selected: " + option)
 print()
 
-print("Please enter a queue name:")
-queue_name = input()
-
-print()
-print(option + ": " + queue_name)
-
 if index == 0:
+    set_email_values()
+    
+if index == 1:
+    queue_name = get_queue_name()
     create_with_deadletter(queue_name)
 
-if index == 1:
+if index == 2:
+    queue_name = get_queue_name()
     purge_then_delete(queue_name)
 
-if index == 2:
+if index == 3:
+    queue_name = get_queue_name()
     send_message(queue_name)
 
-if index == 3:
+if index == 4:
+    queue_name = get_queue_name()
     print("Enter receive count:")
     receive_count = int(input())
     receive_messages(queue_name, receive_count)
